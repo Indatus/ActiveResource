06d5a379053f526b43f3bd284efd603ea83bd66e
Brian Webb <bwebb@indatus.com>
2013-10-28 12:32:25 -0400

    updated docs

--- src/Indatus/ActiveResource/ActiveResource.php
+++ src/Indatus/ActiveResource/ActiveResource.php
@@ -1,4 +1,45 @@
-<?php namespace Indatus\ActiveResource;
+<?php 
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * LICENSE: The BSD 3-Clause
+ * 
+ * Copyright (c) 2013, Indatus
+ * 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, 
+ * are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this list 
+ * of conditions and the following disclaimer.
+ * 
+ * Redistributions in binary form must reproduce the above copyright notice, this list 
+ * of conditions and the following disclaimer in the documentation and/or other 
+ * materials provided with the distribution.
+ * 
+ * Neither the name of Indatus nor the names of its contributors may be used 
+ * to endorse or promote products derived from this software without specific prior 
+ * written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT 
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package     ActiveResource
+ * @author      Brian Webb <bwebb@indatus.com>
+ * @copyright   2013 Indatus
+ * @license     http://opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause
+ */
+
+namespace Indatus\ActiveResource;
 
 use Guzzle\Http\Client;
 use Doctrine\Common\Inflector\Inflector;
@@ -63,7 +104,9 @@ class ActiveResource
      * <code>
      * Employee::$nestedUnder = 'Company:100';
      * $e = Employee::find(1);
+     * </code>
      *
+     * <code>
      * //this would hit /companies/100/employees/1
      * </code>
      * 
@@ -469,6 +512,7 @@ class ActiveResource
      * 
      * @param  string $baseUri        The protocol + host
      * @param  string $path           The URI path after the host
+     * @param  string $http_method    The HTTP method to use for the request (GET, PUT, POST, DELTE etc.)
      * @param  array  $requestHeaders Any additional headers for the request
      * @return  
      */
